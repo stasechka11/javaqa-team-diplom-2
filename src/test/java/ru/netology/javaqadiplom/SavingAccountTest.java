2d5312a1e567e97e50483718988451335894e42d
MihhFor~ <fr.mikle@ya.ru>
2024-01-15 22:36:40 +0300

    add tests to SavingAccount

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -16,6 +16,130 @@ public class SavingAccountTest {
 
         account.add(3_000);
 
+
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
+
+
+    @Test
+    public void shouldThrowIllegalArgumentExceptionWhenSavingRateNegative() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SavingAccount account = new SavingAccount(
+                    1000,
+                    200,
+                    10000,
+                    -15
+
+
+            );
+        });
+    }
+
+    @Test
+    public void FalseWhenAccountNullAfterPay() {
+        SavingAccount account = new SavingAccount(
+                200,
+                100,
+                500,
+                15);
+
+        boolean actual = account.pay(200);
+
+
+        Assertions.assertFalse(actual);
+
+    }
+
+    @Test
+    public void FalseWhenAccountNegativeBalaceAfterPay() {
+        SavingAccount account = new SavingAccount(
+                100,
+                100,
+                500,
+                15);
+
+        boolean actual = account.pay(200);
+
+
+        Assertions.assertFalse(actual);
+
+    }
+
+    @Test
+    public void TrueWhenAccountMoreMinBalanceAfterPay() {
+        SavingAccount account = new SavingAccount(
+                2000,
+                100,
+                5000,
+                15);
+
+        boolean actual = account.pay(200);
+
+
+        Assertions.assertTrue(actual);
+
+    }
+
+    @Test
+    public void FalseWhenAccountNegativeAfterAdd() {
+        SavingAccount account = new SavingAccount(
+                200,
+                100,
+                500,
+                15);
+
+        boolean actual = account.add(-100);
+
+
+        Assertions.assertFalse(actual);
+
+    }
+
+    @Test
+    public void FalseWhenAccountNullAfterAdd() {
+        SavingAccount account = new SavingAccount(
+                200,
+                100,
+                500,
+                15);
+
+        boolean actual = account.add(0);
+
+
+        Assertions.assertFalse(actual);
+
+    }
+
+    @Test
+    public void TrueWhenAmountPlusBalanceLessMinBalanceAfterAdd() {
+        SavingAccount account = new SavingAccount(
+                2000,
+                100,
+                5000,
+                15);
+
+        boolean actual = account.add(200);
+
+
+        Assertions.assertTrue(actual);
+
+    }
+
+    @Test
+    public void shouldCountPercentYear() {
+        SavingAccount account = new SavingAccount(
+                200,
+                100,
+                3500,
+                15
+        );
+
+     int actual = account.yearChange();
+
+        Assertions.assertEquals(30,actual );
+
+    }
+
+
 }
